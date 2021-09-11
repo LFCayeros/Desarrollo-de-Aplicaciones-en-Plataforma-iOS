f0e99cccd40617582c829f618074d79e4cf8b424
LFCayeros <89171475+LFCayeros@users.noreply.github.com>
2021-09-11 00:54:45 -0500

    Actividad 3

---
+++ Actividad 3_LFCC.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+var edad = 23;
+var estatura = 1.79;
+var nombre = "Luis Fernando";
+var apellido:String = "Cayeros";
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var diasSemana = [String:String]()
+diasSemana["1"] = "Lunes";
+diasSemana["2"] = "Martes";
+diasSemana["3"] = "Miercoles";
+diasSemana["4"] = "Jueves";
+diasSemana["5"] = "Viernes";
+diasSemana["6"] = "Sabado";
+diasSemana["7"] = "Domingo";
